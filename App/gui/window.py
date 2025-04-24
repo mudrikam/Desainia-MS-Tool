88cac68d35a04d7acd3487f465906ba7edd3833a
mudrikam <mudrikulh@gmail.com>
2025-04-24 21:13:24 +0700

    Plis Bisa

--- App/gui/window.py
+++ App/gui/window.py
@@ -1,6 +1,6 @@
 from PyQt6.QtWidgets import QMainWindow, QLabel, QApplication
 from PyQt6.QtCore import Qt
-from PyQt6.QtGui import QScreen
+from PyQt6.QtGui import QScreen, QFont
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -8,8 +8,8 @@ class MainWindow(QMainWindow):
         
         self.setWindowTitle("Desainia MS Tool")
         
-        # Set fixed size 800x600
-        self.setFixedSize(800, 600)
+        # Set initial size (but allow resizing)
+        self.resize(800, 600)
         
         # Center window on screen
         screen = QApplication.primaryScreen()
@@ -21,4 +21,8 @@ class MainWindow(QMainWindow):
         # Create and set up Hello World label
         label = QLabel("Hello World", self)
         label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        # Set larger font size
+        font = QFont()
+        font.setPointSize(24)  # Increase font size to 24pt
+        label.setFont(font)
         self.setCentralWidget(label)
